285f41f5424235d83d892afdcec44b129318da33
SandraYushko <shyria6@gmail.com>
2023-10-15 23:46:12 +0300

    work with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
 
 function App() {
     const [value, setValue] = useState<number>(0)
-
-    const incHandler = () => {
-        setValue(value+1)
-    }
-    const setToLocalStorageHandler = () => {
+    useEffect(() => {
         localStorage.setItem('counterValue',JSON.stringify(value))
-    }
-    const getFromLocalStorageHandler = () => {
+    }, [value])
+    useEffect(() => {
         let valueAsString = localStorage.getItem('counterValue')
         if (valueAsString) {
             let newValue = JSON.parse(valueAsString)
             setValue(newValue)
         }
+    }, [])
+
+    const incHandler = () => {
+        setValue(value+1)
     }
+
+
     const clearLocalStorageHandler = () => {
         localStorage.clear()
         setValue(0)
-    }
-    const removeLocalStorageHandler = () => {
-        localStorage.removeItem('counterValue')
     }
 
     return (
@@ -32,13 +31,36 @@ function App() {
             <h1>{value}</h1>
             <div className="button-container">
                 <Button name={'inc'} callback={incHandler}/>
-                <Button name={'set'} callback={setToLocalStorageHandler}/>
-                <Button name={'get'} callback={getFromLocalStorageHandler}/>
                 <Button name={'clear'} callback={clearLocalStorageHandler}/>
-                <Button name={'remove'} callback={removeLocalStorageHandler}/>
             </div>
         </div>
     );
 }
 
 export default App;
+
+
+
+
+//-----------------------------------------------------------------------
+/*const incHandler = () => {
+    setValue(value+1)
+}
+const setToLocalStorageHandler = () => {
+    localStorage.setItem('counterValue',JSON.stringify(value))
+}
+const getFromLocalStorageHandler = () => {
+    let valueAsString = localStorage.getItem('counterValue')
+    if (valueAsString) {
+        let newValue = JSON.parse(valueAsString)
+        setValue(newValue)
+    }
+}
+const clearLocalStorageHandler = () => {
+    localStorage.clear()
+    setValue(0)
+}
+const removeLocalStorageHandler = () => {
+    localStorage.removeItem('counterValue')
+}*/
+
