5e0139d1e0e7a8c8838f2884bc350dc054555cbd
SandraYushko <shyria6@gmail.com>
2023-10-16 23:23:19 +0300

    work with useEffect day 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
 
 function App() {
     const [value, setValue] = useState<number>(0)
+
     useEffect(() => {
-        localStorage.setItem('counterValue',JSON.stringify(value))
+        if (value) {
+            localStorage.setItem('counterValue',JSON.stringify(value))
+        }
     }, [value])
     useEffect(() => {
         let valueAsString = localStorage.getItem('counterValue')
@@ -17,29 +19,30 @@ function App() {
     }, [])
 
     const incHandler = () => {
-        setValue(value+1)
+        setValue(value + 1)
     }
-
-
     const clearLocalStorageHandler = () => {
         localStorage.clear()
         setValue(0)
     }
 
+    const setIntervalLocalStorageHandler = () => {
+
+    }
     return (
         <div className="App">
             <h1>{value}</h1>
             <div className="button-container">
                 <Button name={'inc'} callback={incHandler}/>
-                <Button name={'clear'} callback={clearLocalStorageHandler}/>
+                <Button name={'reset'} callback={clearLocalStorageHandler}/>
+
+                <Button name={'set'} callback={setIntervalLocalStorageHandler}/>
             </div>
         </div>
     );
 }
 
 export default App;
-
-
 
 
 //-----------------------------------------------------------------------
@@ -64,3 +67,24 @@ const removeLocalStorageHandler = () => {
     localStorage.removeItem('counterValue')
 }*/
 
+//______________________________________________________________________
+
+/*
+useEffect(() => {
+    localStorage.setItem('counterValue',JSON.stringify(value))
+}, [value])
+useEffect(() => {
+    let valueAsString = localStorage.getItem('counterValue')
+    if (valueAsString) {
+        let newValue = JSON.parse(valueAsString)
+        setValue(newValue)
+    }
+}, [])
+
+const incHandler = () => {
+    setValue(value+1)
+}
+const clearLocalStorageHandler = () => {
+    localStorage.clear()
+    setValue(0)
+}*/
